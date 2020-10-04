141f83e82d9905e2be61a9cf2c3519fe292de570
shahid89 <shahid89@dh2026pc03.utm.utoronto.ca>
2020-10-04 03:05:41 -0400

    Completed Shard functionality

--- proxy/Shard.java
+++ proxy/Shard.java
@@ -20,23 +20,27 @@ class Shard {
         this.currentHost = (this.currentHost + 1) % this.hosts.size();
     }
 
+    private String getCurrentHost() {
+        return this.hosts.get(this.currentHost);
+    }
+
     public Socket forwardReadRequest(byte[] request, int bytesRead, int nodePort) {
-        System.out.print(String.format("Shard %d received read request, ", this.shardNumber));
+        System.out.println(String.format("Shard %d received read request", this.shardNumber));
         OutputStream streamToNode;
         Socket nodeSocket;
         String host;
 
         // Iterate through hosts until able to make successful connection
         while (true) {
+            this.setNextHost();
             try {
-                this.setNextHost();
-                host = this.hosts.get(currentHost);
+                host = this.getCurrentHost();
                 nodeSocket = new Socket(host, nodePort);
                 streamToNode = nodeSocket.getOutputStream();
-                System.out.print(String.format("forwarding to host %s\n", this.shardNumber, host));
+                System.out.println(String.format(" ✅ ACCEPTED: %s\n", host));
                 break;
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println(String.format(" ❌ UNAVAILABLE: %s\n", this.getCurrentHost()));
             }
         }
 
@@ -54,21 +58,16 @@ class Shard {
 
     public Socket forwardWriteRequest(byte[] request, int bytesRead, int nodePort) {
         ArrayList<Socket> nodeSockets = new ArrayList<Socket>();
-        System.out.print(String.format("Shard %d received write request, forwarding to hosts", this.shardNumber));
+        System.out.println(String.format("Shard %d received write request, forwarding to hosts:", this.shardNumber));
         for (String host : this.hosts) {
-            System.out.print(String.format(" %s", host));
+            System.out.print(String.format("  • %s", host));
             try {
                 nodeSockets.add(new Socket(host, nodePort));
-            } catch (UnknownHostException e) {
-                System.out.print(":UNAVAILABLE");
-                e.printStackTrace();
+                System.out.print(": ✅ ACCEPTED\n");
             } catch (IOException e) {
-                System.out.print(":UNAVAILABLE");
-                e.printStackTrace();
+                System.out.print(": ❌ UNAVAILABLE\n");
             }
         }
-        System.out.print(".\n");
-
         for (Socket nodeSocket : nodeSockets) {
             try {
                 OutputStream streamToNode = nodeSocket.getOutputStream();
