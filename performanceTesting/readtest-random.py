048eb65d425574c0e31f97a03a5a52a4ba96f849
Ali Raza <razaal14>
2020-10-07 01:25:27 -0400

    some testing with random shorts

--- performanceTesting/readtest-random.py
+++ performanceTesting/readtest-random.py
@@ -3,27 +3,36 @@
 import random, string, subprocess
 import requests
 
-def test():
+import random
+import string
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def test(n):
 	response1 = {"success": 0, "fail": 0}
 	response2 = {"success": 0, "fail": 0}
 	response3 = {"success": 0, "fail": 0}
-	for i in range(10):
+	for i in range(n):
 		# print(request)
 		# subprocess.call(["curl", "-X", "GET", request], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-		request1 = requests.get("http://dh2020pc05:8030/c")
+		request1 = requests.get("http://dh2020pc05:8030/"+get_random_string(8))
 		if request1.status_code == 404:
 			response1["success"] += 1
 		else:
 			response1["fail"] += 1
 		
-		request2 = requests.get("http://dh2020pc05:8030/b")
+		request2 = requests.get("http://dh2020pc05:8030/"+get_random_string(8))
 		if request2.status_code == 404:
 			response2["success"] += 1
 		else:
 			response2["fail"] += 1
 		
-		request3 = requests.get("http://dh2020pc05:8030/gg")
-		if request3.status_code == 200:
+		request3 = requests.get("http://dh2020pc05:8030/"+get_random_string(8))
+		if request3.status_code == 404:
 			response3["success"] += 1
 		else:
 			response3["fail"] += 1
@@ -34,7 +43,7 @@ def test():
 
 if __name__=='__main__':
 	import time
-	start = time.time()
-	test()
-	end = time.time()
-	print(end - start)
+	tic = time.perf_counter()
+	test(1000)
+	toc = time.perf_counter()
+	print(f"Took {toc - tic:0.4f} seconds")
