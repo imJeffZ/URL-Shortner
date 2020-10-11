7d324f24e59a892332b1c608a22391889ba25aa1
Jefferson Zhong <zhongjef@dh2020pc09.utm.utoronto.ca>
2020-10-11 03:50:50 -0400

    Implemented random and cache testing

---
+++ performanceTesting/random_tests.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+import random, string
+import requests
+import threading
+
+PROXY = "http://dh2020pc05:8030/"
+
+
+
+class testThread(threading.Thread):
+	def __init__(self, thread_id, read_tests, write_tests):
+		threading.Thread.__init__(self)
+		self.thread_id = thread_id
+		self.read_tests = read_tests
+		self.write_tests = write_tests
+	
+	def run(self):
+		GETResponse = send_GET_Request(self.read_tests)
+		print("read response from thread" + str(self.thread_id) + ": " + str(GETResponse))
+		PUTResponse = send_PUT_Request(self.write_tests)
+		print("write response from thread" + str(self.thread_id) + ": " + str(PUTResponse))
+
+def get_random_string(length):
+	letters = string.ascii_lowercase
+	result_str = ''.join(random.choice(letters) for i in range(length))
+	return result_str
+
+def send_GET_Request(num_tests):
+	response = {"success": 0, "fail": 0}
+
+	for i in range(num_tests):
+		try:
+			request = requests.get(PROXY + get_random_string(8))
+			if request.status_code == 404:
+				response["success"] += 1
+			else:
+				response["fail"] += 1
+		except:
+			print("Error sending request")
+	return response
+
+def send_PUT_Request(num_tests):
+	response = {"success": 0, "fail": 0}
+
+	for i in range(num_tests):
+		longResource = "http://" + get_random_string(20)
+		shortResource = get_random_string(8)
+		try:
+			request = requests.put(PROXY + "?short=" + shortResource + "&long=" + longResource)
+			if request.status_code == 200:
+				response["success"] += 1
+			else:
+				response["fail"] += 1
+		except:
+			print("Error sending request")
+
+	return response
+
+def test(num_threads, total_read_tests, total_write_tests):
+	threads = []
+	read_tests_per_thread = total_read_tests // num_threads
+	write_tests_per_thread = total_write_tests // num_threads
+	for thread_id in range(num_threads):
+		threads.append(testThread(thread_id, read_tests_per_thread, write_tests_per_thread))
+	for t in threads:
+		t.start()
+	for t in threads:
+		t.join()
+
+
+if __name__=='__main__':
+	import time
+	import sys
+	
+	if len(sys.argv) != 4:
+		print("Usage: python3 random_tests.py [proxy_host] [number_of_read_tests] [number_of_write_tests]")
+		sys.exit(1)
+	
+	proxy_host = sys.argv[1]
+	PROXY = "http://" + proxy_host + ":8030/"
+	read_tests = int(sys.argv[2])
+	write_tests = int(sys.argv[3])
+	num_threads = 8
+
+	tic = time.perf_counter()
+	test(num_threads, read_tests, write_tests)
+	toc = time.perf_counter()
+	print(f"{read_tests:d} read tests and {write_tests:d} write tests took {toc - tic:0.4f} seconds on {num_threads:d} threads")
